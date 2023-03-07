b8a1c8b0f61b359e0e22dde955d1626083e4326d
Sarah <ac22915@bristol.ac.uk>
2023-03-07 21:17:13 +0000

    tools review 0.5

---
+++ tools/code/8_2.sql
@@ -0,0 +1,38 @@
+SELECT name FROM Party ORDER BY name ASC;
+
+SELECT Party.name FROM Party 
+    INNER JOIN Candidate 
+    ON Party.id = Candidate.party
+    INNER JOIN Ward
+    ON Ward.id = Candidate.ward
+    WHERE Ward.name = "Bedminster";
+
+SELECT SUM(Candidate.votes)
+    FROM Party
+    INNER JOIN Candidate 
+    ON Party.id = Candidate.party
+    INNER JOIN Ward
+    ON Ward.id = Candidate.ward
+    WHERE Ward.name = "Stockwood"
+    AND
+    Party.name = "labour";
+
+SELECT Candidate.name, Party.name, Candidate.votes
+    FROM Party
+    INNER JOIN Candidate 
+    ON Party.id = Candidate.party
+    INNER JOIN Ward
+    ON Ward.id = Candidate.ward
+    WHERE Ward.name = "Southville"
+    ORDER BY Candidate.votes
+    DESC;
+
+SELECT Candidate.name, Party.name, Candidate.votes
+    FROM Party
+    INNER JOIN Candidate 
+    ON Party.id = Candidate.party
+    INNER JOIN Ward
+    ON Ward.id = Candidate.ward
+    WHERE Ward.name = "Knowle"
+    ORDER BY Candidate.votes DESC
+    LIMIT 1;
